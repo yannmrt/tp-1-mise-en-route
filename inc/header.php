8d37522c22c2670119a159d10143b4e01b65e32a
Yann martin <ymartin@la-providence.net>
2021-09-13 09:43:57 +0200

    CSS PWreset

---
+++ inc/header.php
@@ -0,0 +1,22 @@
+<!-- Responsive navbar-->
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="container">
+                <a class="navbar-brand" href="#">TP 1 - Mise en route</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                        <li class="nav-item"><a class="nav-link active" aria-current="page" href="index.php">Accueil</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#">Voir la map</a></li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Espace membre</a>
+                            <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                <li><a class="dropdown-item" href="login.php">Connexion</a></li>
+                                <li><a class="dropdown-item" href="register.php">Inscription</a></li>
+                                <li><hr class="dropdown-divider" /></li>
+                                <li><a class="dropdown-item" href="pwreset.php">Mot de passe oublié ?</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
--- pwreset.php
+++ pwreset.php
@@ -21,22 +21,58 @@ if(isset($_POST["username"])) {
 
 ?>
 
-<!doctype html>
+<!DOCTYPE html>
 <html lang="fr">
     <head>
-        <title>Mot de passe oublié ?</title>
         <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Mot de passe oublié ?</title>
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
     </head>
-<body>
+    <body>
+        <?php include("inc/header.php"); ?> 
+        <!-- Page content-->
+        <div class="container">
+            <div class="text-center mt-5">
+                <h1>Mot de passe oublié ?</h1>
+                <p class="lead">Veuillez entrer votre nom d'utilisateur ainsi que votre phrase de récupération.</p>
+            </div>
 
-<form method="POST" action="">
-    <input type="text" name="username" placeholder="Nom d'utilisateur" />
-    <input type="password" name="securityPhrase" placeholder="Phrase de sécurité" />
+            <!-- Form connexion -->
+            <form method="POST" action="">
+                <div align="center">
+                    <div class="form-group">
+                        <label for="username">Nom d'utilisateur</label>
+                        <div class="col-sm-4">
+                            <input type="text" class="form-control" id="username"name="username" placeholder="Votre nom d'utilisateur">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Phrase de sécurité</label>
+                        <div class="col-sm-4">
+                            <input type="password" class="form-control" id="securityPhrase" name="securityPhrase" placeholder="Votre phrase de récupération">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Nouveau mot de passe</label>
+                        <div class="col-sm-4">
+                            <input type="password" class="form-control" id="newpassword" name="newpassword" placeholder="Votre nouveau mot de passe">
+                            <small>Tache de ne pas l'oublier de nouveau</small>
+                        </div>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Changer mon mot de passe</button>
+                </div>
+            </form>
 
-    <input type="password" name="newpassword" placeholder="Nouveau mot de passe" />
-
-    <input type="submit" value="Changer mot de passe" />
-</form>
-
-</body>
+        </div>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
 </html>
