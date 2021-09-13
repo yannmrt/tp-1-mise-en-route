0df594dab3e78c3acf37a65367a62e798e85b53a
Yann martin <ymartin@la-providence.net>
2021-09-13 10:27:07 +0200

    Edit class and create ajoutTrame function

--- class/admin.php
+++ class/admin.php
@@ -1,41 +1,62 @@
 <?php 
 
-class Admin extends User
-{
+class Admin {
 
     /**
      *
      *  $_admin : valeur admin de la bdd
+     *  $_longitude : longitude de la tram
+     *  $_latitude : latitude de la tram 
+     *  $_idGPS : id de la tram GPS
      *  
     */
 
     private $_admin; 
+    private $_longitude;
+    private $_latitude;
+    private $_idGPS;
+    private $_name;
 
+    private $_db;
 
-    /**
-     *  $_PDO : variable PDO (inc/db.php) 
-    */
-    //Pas de constucteur : on prend celui de User par héritage
-
-    /**
-     *  $
-    */
+    //Constructeur du User depuis la BDD (utiliser PDO)
+    public function __construct($_PDO) {
+        $this->_db = $_PDO;
+    }
 
     //On ajoute des coordonnée GPS à la base de donnée grace à la longitude et la latitude renseigné
-    public function AjoutTrame($longitude, $latitude) 
-    {
+    public function AjoutTrame($longitude, $latitude, $name) {
+        $this->_longitude = htmlspecialchars($longitude);
+        $this->_latitude = htmlspecialchars($latitude);
+        $this->_name = htmlspecialchars($name);
+
+        if(!empty($this->_longitude) AND !empty($this->_latitude) AND !empty($this->_name)) {
+            $req_name_exist = $this->_db->prepare("SELECT name FROM gps WHERE name = ?");
+            $req_name_exist->execute(array($this->_name));
+            $name_exist_count = $req_name_exist->rowCount();
+
+            if($name_exist_count == 0) {
+                $add_trame =  $this->_db->prepare("INSERT INTO gps SET longitude = :longitude, latitude = :latitude, name = :name");
+                $add_trame->execute(array(
+                    "longitude" => $this->_longitude,
+                    "latitude" => $this->_latitude,
+                    "name" => $this->_name
+                ));
+            } else {
+                $error = "Le nom est déjà utilisé.";
+                return $error;
+            }
+        }
 
     }
 
     //On supprime un points GPS grace à son ID (faire système pour trouver / lister toutes les trames ?)
-    public function DeleteTrame($idGPS)
-    {
+    public function DeleteTrame($idGPS) {
 
     }
 
     //On modifie la longitude / latitude GPS identifié grace à son ID
-    public function ModifTrame($idGPS, $longitude, $latitude)
-    {
+    public function ModifTrame($idGPS, $longitude, $latitude) {
 
     }
 } 
