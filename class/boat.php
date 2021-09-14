fcaf1f0bf004d2a0bbc1164e80071adb472c5fe0
Yann martin <ymartin@la-providence.net>
2021-09-14 09:46:29 +0200

    add boat comm

--- class/boat.php
+++ class/boat.php
@@ -1,6 +1,6 @@
 <?php 
 
-class Bateau {
+class Boat {
 
     /**
      * 
@@ -18,6 +18,60 @@ class Bateau {
     // Constructeur de la classe User avec la base de donnée ($_PDO est dans /inc/db.php)
     public function __construct($_PDO) {
         $this->_db = $_PDO;
+    }
+
+    /**
+     * 
+     * On ajoute un bateau a la base de donnée (page: admin/addBoat.php)
+     * 
+     * $name : nom du bateau
+     * 
+     */
+    public function addBoat($name) {
+
+    }
+
+    /**
+     * 
+     * On modifier les informations du bateau par l'id  (page: admin/editBoat.php)
+     * 
+     * $name : nom du bateau
+     * $id : id du bateau
+     * 
+     */
+    public function editBoat($name, $id) {
+
+    }
+
+    /**
+     * 
+     * Supprimer un bateau en fonction de l'id (page: admin/editBoat.php)
+     * 
+     * $id : id du bateau
+     * 
+     */
+    public function delBoat($id) {
+
+    }
+
+    /**
+     * 
+     * On affiche la liste des bateaux de la bdd dans un tableau (page: admin/boatList.php)
+     * 
+     */
+    public function showBoatList() {
+
+    }
+
+    /**
+     * 
+     * On récupère les informations d'un bateau par l'id (page: admin/editBoat.php)
+     * 
+     * $id : id du bateau
+     * 
+     */
+    public function getBoatInfo() {
+        
     }
 
 }
