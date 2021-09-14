8caa837aea65c3a1fa9b7b235dd6ed2223945b4c
Yann martin <ymartin@la-providence.net>
2021-09-14 10:24:49 +0200

    pre beta boat class php

--- class/boat.php
+++ class/boat.php
@@ -5,13 +5,13 @@ class Boat {
     /**
      * 
      * id : id du bateau
-     * nom : nom du bateau
+     * name : nom du bateau
      * 
      * _db : connexion pdo à la bdd 
      * 
      */
     private $_id;
-    private $_nom; 
+    private $_name; 
 
     private $_db;
 
@@ -29,6 +29,25 @@ class Boat {
      */
     public function addBoat($name) {
 
+        $this->_name = htmlspecialchars($name);
+
+        if(!empty($this->_name)) {
+            $req_boat_exist = $this->_db->prepare("SELECT name FROM boat WHERE name = ?");
+            $req_boat_exist->execute(array($this->_name));
+            $boat_name_exist = $req_boat_exist->rowCount();
+            
+            if($boat_name_exist == 0) {
+                $add_boat = $this->_db->prepare("INSERT INT boat SET name = :name");
+                $add_boat->execute(array($this->_name));
+
+                $error = '<div class="alert alert-success" role="alert">Le bateau a été ajouté.</div>';
+                return $error;
+
+            } else {
+                $error = '<div class="alert alert-danger" role="alert">Ce nom de bateau est déjà utilisé.</div>';
+                return $error;
+            }
+        }
     }
 
     /**
@@ -41,6 +60,16 @@ class Boat {
      */
     public function editBoat($name, $id) {
 
+        $this->_name = htmlspecialchars($name);
+        $this->_id = htmlspecialchars($id);
+
+        if(!empty($this->_name) AND !empty($this->_id)) {
+            $editBoat = $this->_db->prepare("UPDATE boat SET name = ? WHERE id = ?");
+            $editBoat->execute(array($this->_name, $this->_id));
+
+            $error = '<div class="alert alert-success" role="alert">Le bateau a bien été modifié.</div>';
+            return $error;
+        }
     }
 
     /**
@@ -52,6 +81,14 @@ class Boat {
      */
     public function delBoat($id) {
 
+        $this->_id = htmlspecialchars($id);
+
+        if(!empty($this->_id)) {
+            $delBoat = $this->_db->prepare("DELETE FROM boat WHERE id = ?");
+            $delBoat->execute(array($this->_id));
+
+            header("Location: boatList.php");
+        }
     }
 
     /**
@@ -61,6 +98,22 @@ class Boat {
      */
     public function showBoatList() {
 
+        $req_boatList = $this->_db->prepare("SELECT * FROM boat");
+        $req_noatList->execute();
+        $count = $req_noatList->rowCount();
+
+        while($_BOAT = $req_noatList->fetch()) {
+            echo '
+            
+            <tr>
+                <th scope="row">'.$_BOAT["id"].'</th>
+                <td>'.$_BOAT["name"].'</td>
+                <td><a href="editTrame.php?id='.$_BOAT["id"].'"><label class="btn btn-primary btn-sm">Modifier</label></a><a href="editTrame.php?id='.$_BOAT["id"].'&method=delete"><label class="btn btn-danger btn-sm">Supprimer</label></a></td>
+            </tr>
+            
+            ';
+
+        }
     }
 
     /**
@@ -72,6 +125,10 @@ class Boat {
      */
     public function getBoatInfo() {
         
+        $get_info = $this->_db->prepare("SELECT * FROM boat WHERE id = ?");
+        $get_info->execute(array($id)); 
+        
+        return $_infoBoat = $get_info->fetch();
     }
 
 }
