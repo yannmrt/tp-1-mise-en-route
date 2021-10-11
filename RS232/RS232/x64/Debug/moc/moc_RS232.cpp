fbbb04a5d0f0c51c83f7901b3594da512ea219c6
Yann martin <ymartin@la-providence.net>
2021-10-11 11:41:55 +0200

    prog fonctionnel

--- RS232/RS232/x64/Debug/moc/moc_RS232.cpp
+++ RS232/RS232/x64/Debug/moc/moc_RS232.cpp
@@ -22,8 +22,8 @@ QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_RS232_t {
-    QByteArrayData data[15];
-    char stringdata0[117];
+    QByteArrayData data[9];
+    char stringdata0[81];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -33,25 +33,19 @@ struct qt_meta_stringdata_RS232_t {
 static const qt_meta_stringdata_RS232_t qt_meta_stringdata_RS232 = {
     {
 QT_MOC_LITERAL(0, 0, 5), // "RS232"
-QT_MOC_LITERAL(1, 6, 5), // "issue"
-QT_MOC_LITERAL(2, 12, 0), // ""
-QT_MOC_LITERAL(3, 13, 5), // "trame"
-QT_MOC_LITERAL(4, 19, 7), // "receive"
-QT_MOC_LITERAL(5, 27, 11), // "decodeTrame"
-QT_MOC_LITERAL(6, 39, 10), // "getTrameDb"
-QT_MOC_LITERAL(7, 50, 10), // "addTrameDb"
-QT_MOC_LITERAL(8, 61, 8), // "latitude"
-QT_MOC_LITERAL(9, 70, 9), // "longitude"
-QT_MOC_LITERAL(10, 80, 10), // "horodatage"
-QT_MOC_LITERAL(11, 91, 4), // "name"
-QT_MOC_LITERAL(12, 96, 6), // "idBoat"
-QT_MOC_LITERAL(13, 103, 10), // "delTrameDb"
-QT_MOC_LITERAL(14, 114, 2) // "id"
+QT_MOC_LITERAL(1, 6, 14), // "serialPortRead"
+QT_MOC_LITERAL(2, 21, 0), // ""
+QT_MOC_LITERAL(3, 22, 11), // "decodeTrame"
+QT_MOC_LITERAL(4, 34, 5), // "trame"
+QT_MOC_LITERAL(5, 40, 10), // "addTrameDb"
+QT_MOC_LITERAL(6, 51, 8), // "latitude"
+QT_MOC_LITERAL(7, 60, 9), // "longitude"
+QT_MOC_LITERAL(8, 70, 10) // "horodatage"
 
     },
-    "RS232\0issue\0\0trame\0receive\0decodeTrame\0"
-    "getTrameDb\0addTrameDb\0latitude\0longitude\0"
-    "horodatage\0name\0idBoat\0delTrameDb\0id"
+    "RS232\0serialPortRead\0\0decodeTrame\0"
+    "trame\0addTrameDb\0latitude\0longitude\0"
+    "horodatage"
 };
 #undef QT_MOC_LITERAL
 
@@ -61,7 +55,7 @@ static const uint qt_meta_data_RS232[] = {
        8,       // revision
        0,       // classname
        0,    0, // classinfo
-       6,   14, // methods
+       3,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -69,20 +63,14 @@ static const uint qt_meta_data_RS232[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags
-       1,    1,   44,    2, 0x0a /* Public */,
-       4,    0,   47,    2, 0x0a /* Public */,
-       5,    1,   48,    2, 0x0a /* Public */,
-       6,    0,   51,    2, 0x0a /* Public */,
-       7,    5,   52,    2, 0x0a /* Public */,
-      13,    1,   63,    2, 0x0a /* Public */,
+       1,    0,   29,    2, 0x0a /* Public */,
+       3,    1,   30,    2, 0x0a /* Public */,
+       5,    3,   33,    2, 0x0a /* Public */,
 
  // slots: parameters
-    QMetaType::Void, QMetaType::QString,    3,
     QMetaType::Void,
-    QMetaType::Void, QMetaType::QString,    3,
-    QMetaType::Void,
-    QMetaType::Void, QMetaType::QString, QMetaType::QString, QMetaType::QString, QMetaType::QString, QMetaType::QString,    8,    9,   10,   11,   12,
-    QMetaType::Void, QMetaType::QString,   14,
+    QMetaType::Void, QMetaType::QString,    4,
+    QMetaType::Void, QMetaType::QString, QMetaType::QString, QMetaType::QString,    6,    7,    8,
 
        0        // eod
 };
@@ -93,12 +81,9 @@ void RS232::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void 
         auto *_t = static_cast<RS232 *>(_o);
         Q_UNUSED(_t)
         switch (_id) {
-        case 0: _t->issue((*reinterpret_cast< const QString(*)>(_a[1]))); break;
-        case 1: _t->receive(); break;
-        case 2: _t->decodeTrame((*reinterpret_cast< const QString(*)>(_a[1]))); break;
-        case 3: _t->getTrameDb(); break;
-        case 4: _t->addTrameDb((*reinterpret_cast< QString(*)>(_a[1])),(*reinterpret_cast< QString(*)>(_a[2])),(*reinterpret_cast< QString(*)>(_a[3])),(*reinterpret_cast< QString(*)>(_a[4])),(*reinterpret_cast< QString(*)>(_a[5]))); break;
-        case 5: _t->delTrameDb((*reinterpret_cast< QString(*)>(_a[1]))); break;
+        case 0: _t->serialPortRead(); break;
+        case 1: _t->decodeTrame((*reinterpret_cast< const QString(*)>(_a[1]))); break;
+        case 2: _t->addTrameDb((*reinterpret_cast< const QString(*)>(_a[1])),(*reinterpret_cast< const QString(*)>(_a[2])),(*reinterpret_cast< const QString(*)>(_a[3]))); break;
         default: ;
         }
     }
@@ -133,13 +118,13 @@ int RS232::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 6)
+        if (_id < 3)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 6;
+        _id -= 3;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 6)
+        if (_id < 3)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 6;
+        _id -= 3;
     }
     return _id;
 }
